3ecb055fce0d45eb47e8ab2287d5281790dff1a8
sashsutton <sashasuttons3@gmail.com>
2025-12-05 13:08:34 +0100

    data appears in the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+
+const API_URL = 'http://127.0.0.1:8000'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch(`${API_URL}/data`)
+      const crisisData = await res.json()
+      setData(crisisData)
+    }catch(error){
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  useEffect(() => { fetchData() }, [])
 
   return (
-    <>
-    <h1> Hello world</h1>
-    </>
+    <div className="container">
+      <h1>The crisis flow</h1>
+      <div className="card">
+        <h2>The visualisation</h2>
+
+        {data.map((item) => (
+          <div key={item.id} className="item">
+            <h3>{item.id}</h3>
+            <h3>{item.location}</h3>
+            <h3>{item.pca_x}</h3>
+            <h3>{item.pca_y}</h3>
+            <h3>{item.cluster_id}</h3>
+
+            <p>{item.text}</p>
+          </div>
+        ))}
+
+      </div>
+
+    </div>
   )
 }
 
